59e6c0b532343f3609217ad46a64c9d058189880
Svyatoslav Deinekin <svyadey@mail.ru>
2024-02-19 00:31:50 +0300

    hw_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(25),
+	last_name varchar(25),
+	title varchar(50),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id int PRIMARY KEY,
+	company_name varchar(50),
+	contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10),
+	employee_id int,
+	order_date date,
+	ship_city varchar(20)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+import os
+
+
+def write_csv_to_sql_table(filename: str, table_name: str) -> None:
+    """
+    Функция для записи данных из .csv в postgresql
+
+    :param filename: путь до .csv файла, откуда берем данные
+    :param table_name: название таблицы в базе данных
+    """
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='wtf5959ololo')
+        cur = conn.cursor()
+        number_of_columns = len(next(reader)) * '%s, '
+        cur.executemany(f"INSERT INTO {table_name} VALUES ({number_of_columns[:-2]})", reader)
+        conn.commit()
+
+
+def main() -> None:
+    """Главная функция, получаем пути до каждого .csv файла и выгружаем из них данные в таблицу"""
+    customer_path = os.path.abspath("north_data/customers_data.csv")
+    employees_path = os.path.abspath("north_data/employees_data.csv")
+    orders_path = os.path.abspath("north_data/orders_data.csv")
+
+    write_csv_to_sql_table(customer_path, "customers")
+    write_csv_to_sql_table(employees_path, "employees")
+    write_csv_to_sql_table(orders_path, "orders")
+
+
+if __name__ == '__main__':
+    main()
